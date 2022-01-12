c104bfa41cd631c9598e9e72ca1c719c334f69de
Juan Gonzalez <jgonzalez@naver.com>
2022-01-12 11:05:52 +0900

    functional state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Header from "./components/Header.js";
+import Tasks from "./components/Tasks.js";
+import AddTask from "./components/AddTask";
 
-function App() {
+const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    }
+    getTasks();
+  }, []);
+
+  // Fetching tasks. Global so can be use in other components
+  const fetchTasks = async () => {
+    const response = await fetch("http://localhost:5000/tasks");
+    const responseData = await response.json();
+    return responseData;
+  };
+
+  // Fetch a single task
+  const fetchTask = async (taskId) => {
+    const response = await fetch(`http://localhost:5000/tasks/${taskId}`);
+    const responseData = await response.json();
+    return responseData;
+  };
+
+  //Add Task
+  const addTask = async (task) => {
+    const response = await fetch('http://localhost:5000/tasks', {
+      method: 'POST', 
+      headers: {'Content-type': 'application/json'},
+      body: JSON.stringify(task)
+    })
+
+    const newTask = await response.json(); // POST returns the added task
+
+    setTasks([...tasks, newTask]);
+
+    // const id = new Date().toISOString(); ID automatically created in json server
+    // const newTask = { ...task, id };
+    // setTasks([...tasks, newTask]);
+  };
+
+  // Delete Task
+
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE'
+    })
+
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+  // Togge Reminder
+
+  const toggleReminder = async (id) => {
+    const fetchedTask = await fetchTask(id);
+    const toggledTask = {...fetchedTask, reminder: !fetchedTask.reminder}
+    const response = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(toggledTask)
+    })
+
+    const data = await response.json();
+
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: data.reminder } : task
+      )
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header
+        title="Task Tracker"
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        <h3>There are currently no tasks</h3>
+      )}
     </div>
   );
-}
+};
 
 export default App;
